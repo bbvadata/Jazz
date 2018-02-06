3d701442e7ff0e29a417e6a57cc2a5e91af2e34a
kaalam <kaalam@dybot.com>
2018-02-06 19:59:44 +0100

    Including running the example from Python 2.7

--- pyjazz/poc_swig/build.sh
+++ pyjazz/poc_swig/build.sh
@@ -3,3 +3,8 @@
 swig -python example.i
 gcc -c -fpic example.c example_wrap.c -I/usr/include/python2.7
 gcc -shared example.o example_wrap.o -o _example.so
+
+python2 -c "import example
+print example.fact(6)
+print example.my_mod(23, 7)
+print example.cvar.My_variable"
