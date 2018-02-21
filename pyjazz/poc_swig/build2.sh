cdd99c148b7af0088c9604b802b16402a9f948fd
kaalam <kaalam@dybot.com>
2018-02-21 19:42:18 +0100

    Swig dependency of libpcre.so hidden in shell to avoid confict with other Linux core commands.

--- pyjazz/poc_swig/build2.sh
+++ pyjazz/poc_swig/build2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
+
 swig -python example.i
 gcc -c -fpic example.c example_wrap.c -I/usr/include/python2.7
 gcc -shared example.o example_wrap.o -o _example.so
