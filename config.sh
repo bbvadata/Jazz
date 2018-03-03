d6ada736b125d339bdf9e507e1237963eb004d2c
kaalam <kaalam@dybot.com>
2018-03-03 14:31:15 +0100

    Gathering info complete.

--- config.sh
+++ config.sh
@@ -1,52 +1,84 @@
 #!/bin/bash
 
-# This script creates:
+if [[ $1 = '--help' ]]; then
+  cat _config_/help_on_config.txt
+  exit 0
+fi
 
-    server/makefile                 // To compile the server with make
-    server/main.dox                 // The landing page of the doxygen site of the server
-    server/src/include/platform.h   // The name of the Linux distro with which it was built as constant LINUX_PLATFORM
-    server/src/include/version.h    // The version string as constant JAZZ_VERSION
-    pyjazz/build.sh                 // To build the Python 2.7 3.x package
-    rjazz/build.sh                  // To build the R package
+printf "(usage: ./config.sh --help)\n\nGathering information ...\n\n"
 
+jazz_pwd=$(pwd)
 
-cwd=$(pwd)
+jazz_version=`cat _config_/version`
+jz_processor=`uname -p`
+jazz_distro1=`cat /etc/*-release | grep '^DISTRIB_ID=' | sed 's/^DISTRIB_ID=//'`
+jazz_distro2=`cat /etc/*-release | grep '^DISTRIB_RELEASE=' | sed 's/^DISTRIB_RELEASE=//'`
+
+if [ -e '_config_/mhd_include_path' ]; then mhd_inclpath=`cat _config_/mhd_include_path`; else mhd_inclpath='/usr/include'; fi
+
+if [ ! -e "$mhd_inclpath/microhttpd.h" ]; then
+  echo "** File $mhd_inclpath/microhttpd.h was not found. **"
+  cat _config_/help_mhd_not_found.txt
+  exit
+fi
+
+if [ -e '_config_/mhd_library_path' ]; then mhd_libpath=`cat _config_/mhd_library_path`
+else
+  if [ -e '/usr/lib/x86_64-linux-gnu/libmicrohttpd.so' ]; then mhd_libpath='/usr/lib/x86_64-linux-gnu'; else mhd_libpath='/usr/lib'; fi
+fi
+
+if [ ! -e "$mhd_libpath/libmicrohttpd.so" ]; then
+  echo "** File $mhd_libpath/libmicrohttpd.so was not found. **"
+  cat _config_/help_mhd_not_found.txt
+  exit 1
+fi
 
 cd server
 
 vpath=`echo src/*`
-jzpat=`echo $vpath | sed 's/\ /\n/g' | grep jazz`
+jzpat=`echo $vpath | sed 's/\ /\n/g' | grep jazz | tr '\n' ' '`
 
-cpps=`find src/ | grep '.*jazz_.*cpp$'`
-objs=`echo $cpps | sed 's/\ /\n/g' | sed 's/.*\(jazz_.*cpp\)$/\1/' | sed 's/cpp/o/'`
+cpps=`find src/ | grep '.*jazz_.*cpp$' | tr '\n' ' '`
+objs=`echo $cpps | sed 's/\ /\n/g' | sed 's/.*\(jazz_.*cpp\)$/\1/' | sed 's/cpp/o/' | tr '\n' ' '`
 
+depends ( )
+{
+  for cpp in $cpps; do
+    obj=`echo $cpp | sed 's/.*\(jazz_.*cpp\)$/\1/' | sed 's/cpp/o/'`
+    hea=`echo $cpp | sed 's/cpp$/h/'`
 
-echo $vpath
-echo $jzpat
-echo $cpps
-echo $objs
+    if [ -e $hea ]; then
+      dep=`grep -rnw $cpp $hea -e '^#include.*\(jazz.*h\)' | sed 's/.*\(jazz.*h\).*/\1/'`
+    else
+      dep=`grep -rnw $cpp -e '^#include.*\(jazz.*h\)' | sed 's/.*\(jazz.*h\).*/\1/'`
+    fi
 
+    echo $obj: $dep
+  done
+}
 
-for cpp in $cpps; do
-  obj=`echo $cpp | sed 's/.*\(jazz_.*cpp\)$/\1/' | sed 's/cpp/o/'`
-  hea=`echo $cpp | sed 's/cpp$/h/'`
+jazz_depends=`depends`
 
-  if [ -e $hea ]; then
-    dep=`grep -rnw $cpp $hea -e '^#include.*\(jazz.*h\)' | sed 's/.*\(jazz.*h\).*/\1/'`
-  else
-    dep=`grep -rnw $cpp -e '^#include.*\(jazz.*h\)' | sed 's/.*\(jazz.*h\).*/\1/'`
-  fi
+cd $jazz_pwd
 
-  echo $obj: $dep
-done
+printf "You will now override:\n\n"
+cat _config_/help_on_config.txt | grep '  - '
+printf "\n"
 
+read -p "Do you want to continue? [y/N] " -n 1 -r
+echo    # (optional) move to a new line
 
-cd $cwd
+if [[ $REPLY =~ ^[Yy]$ ]]; then printf "\n"; else exit 1; fi
 
-if [ ! -e "./curl-7.54.0/lib/.libs/libcurl.so.4.4.0" ]; then
-  echo "File ./curl-7.54.0/lib/.libs/libcurl.so.4.4.0 not found."
-  exit 1
-fi
-
-echo "Finished build.sh"
-exit 0
+echo "jazz_pwd     = $jazz_pwd"
+echo "jazz_version = $jazz_version"
+echo "jz_processor = $jz_processor"
+echo "jazz_distro1 = $jazz_distro1"
+echo "jazz_distro2 = $jazz_distro2"
+echo "mhd_inclpath = $mhd_inclpath"
+echo "mhd_libpath  = $mhd_libpath"
+echo "vpath        = $vpath"
+echo "jzpat        = $jzpat"
+echo "cpps         = $cpps"
+echo "objs         = $objs"
+echo "jazz_depends = $jazz_depends"
