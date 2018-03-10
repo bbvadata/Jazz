2cf01ca5791fc8386cb014a36511fbe8a34d2bcc
kaalam <kaalam@dybot.com>
2018-03-10 21:53:52 +0100

    Implememts the logic for Python2 and Python3 detection

--- py_package/pyjazz/pyjazz/__init__.py
+++ py_package/pyjazz/pyjazz/__init__.py
@@ -1,1 +1,12 @@
+from sys import version_info
+import os
+
+try:
+	os.unlink('pyjazz/_jazz_blocks.so')
+except OSError:
+	pass
+
+if version_info[0] < 3: os.symlink(os.path.abspath('pyjazz/_jazz_blocks2.so'), 'pyjazz/_jazz_blocks.so')
+else:                   os.symlink(os.path.abspath('pyjazz/_jazz_blocks3.so'), 'pyjazz/_jazz_blocks.so')
+
 from pyjazz.get_jazz_version import get_jazz_version
