04a7e6e1f7f4fd000f24e6dcdb037b389dc4e206
kaalam <kaalam@dybot.com>
2018-03-11 18:27:40 +0100

    Completes a functioning installation logic

--- config.sh
+++ config.sh
@@ -199,14 +199,43 @@ printf "Writing: py_package/pyjazz/setup.py ... "
 
 echo "from setuptools import setup
 
+try:
+	with open('README.md', 'r') as ldf:
+		long_desc = ldf.read()
+except FileNotFoundError:
+	with open('README.rst', 'r') as ldf:
+		long_desc = ldf.read().replace('\n', '')
+
 setup(name='pyjazz',
-	version='$jazz_version',
-	description='Official Python client for Jazz',
-	url='http://github.com/kaalam/jazz',
-	author='kaalam.ai',
-	author_email='kaalam@kaalam.ai',
-	license='Apache 2.0',
-	packages=['pyjazz'])" > py_package/pyjazz/setup.py
+	version					= '$jazz_version',
+	description				= 'Official Python client for Jazz',
+	long_description		= long_desc,
+	url						= 'http://github.com/kaalam/jazz',
+	author					= 'kaalam.ai',
+	author_email			= 'kaalam@kaalam.ai',
+	license					= 'Apache 2.0',
+	packages				= ['pyjazz'],
+	package_data			= {'pyjazz': ['_jazz_blocks2.so', '_jazz_blocks3.so']},
+	include_package_data	= True,
+	zip_safe				= False,
+	classifiers			    = [
+		'Development Status :: 2 - Pre-Alpha',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Education',
+		'Intended Audience :: Financial and Insurance Industry',
+		'Intended Audience :: Healthcare Industry',
+		'Intended Audience :: Information Technology',
+		'Intended Audience :: Manufacturing',
+		'Intended Audience :: Science/Research',
+		'Intended Audience :: System Administrators',
+		'Intended Audience :: Telecommunications Industry',
+		'License :: OSI Approved :: Apache Software License',
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Topic :: Scientific/Engineering :: Artificial Intelligence'
+      ],
+	)" > py_package/pyjazz/setup.py
 
 printf "Ok.\n"
 
