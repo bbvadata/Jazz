9863463b0e133637942813c6cc6fa1741fbb2ea1
kaalam <kaalam@dybot.com>
2018-03-29 10:03:35 +0200

    Adds the function ExpandEscapeSequences following the style guide

--- server/src/jazz_elements/jazz_utils.cpp
+++ server/src/jazz_elements/jazz_utils.cpp
@@ -21,12 +21,14 @@
   limitations under the License.
 */
 
-//TODO: Follow the style guide on naming.
-//TODO: Follow the style guide on the use of the namespace.
-//TODO: Move tests to a test file (conditionally included).
+//TODO: Move all the utils and break the logger in functions.
 
+
+#include <map>
+#include <string.h>
 
 #include "src/jazz_elements/jazz_utils.h"
+
 
 /** Count the number of bytes required by an utf-8 string of len characters.
 
@@ -69,6 +71,129 @@ int CountBytesFromUtf8(char *buff, int len)
 	}
 
 	return bytes;
+}
+
+
+/** Expand escaped strings at runtime.
+
+Public Domain by Jerry Coffin.
+
+Interprets a string in a manner similar to that the compiler does string literals in a program.	 All escape sequences are
+longer than their translated equivalant, so the string is translated in place and either remains the same length or becomes shorter.
+
+April 17 tests built and compliant with:
+
+\\a	 07	   Alert (Beep, Bell) (added in C89)[1]
+\\b	 08	   Backspace
+\\f	 0C	   Formfeed
+\\n	 0A	   Newline (Line Feed); see notes below
+\\r	 0D	   Carriage Return
+\\t	 09	   Horizontal Tab
+\\v	 0B	   Vertical Tab
+\\\\ 5C	   Backslash
+\\'	 27	   Single quotation mark
+\\"	 22	   Double quotation mark
+\\?	 3F	   Question mark (used to avoid trigraphs)
+\\nnn	   The byte whose numerical value is given by nnn interpreted as an octal number
+\\xhh	   The byte whose numerical value is given by hh… interpreted as a hexadecimal number
+
+https://en.wikipedia.org/wiki/Escape_sequences_in_C
+*/
+char *ExpandEscapeSequences(char * buff)
+{
+	char        *pt	 = buff;
+	size_t		 len = strlen(buff);
+	unsigned int num, any;
+
+	while (nullptr != (pt = strchr(pt, '\\'))) {
+		int numlen = 1;
+		switch (pt[1]) {
+		case 'a':
+			*pt = '\a';
+			break;
+
+		case 'b':
+			*pt = '\b';
+			break;
+
+		case 'f':
+			*pt = '\f';
+			break;
+
+		case 'n':
+			*pt = '\n';
+			break;
+
+		case 'r':
+			*pt = '\r';
+			break;
+
+		case 't':
+			*pt = '\t';
+			break;
+
+		case 'v':
+			*pt = '\v';
+			break;
+
+		case '\\':
+			break;
+
+		case '\'':
+			*pt = '\'';
+			break;
+
+		case '\"':
+			*pt = '\"';
+			break;
+
+		case '\?':
+			*pt = '\?';
+			break;
+
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+			if (pt[2] && pt[3] && sscanf(&pt[1], "%3o", &num) == 1 && sscanf(&pt[2], "%3o", &any) == 1 && sscanf(&pt[3], "%3o", &any) == 1)
+			{
+				numlen = 3;
+				*pt	 = (char) num;
+			}
+			else
+			{
+				numlen = 1;
+				*pt = pt[1];
+			}
+			break;
+
+		case 'x':
+			if (pt[2] && pt[3] && sscanf(&pt[2], "%2x", &num) == 1 && sscanf(&pt[3], "%2x", &any) == 1)
+			{
+				numlen = 3;
+				*pt	 = (char) num;
+			}
+			else
+			{
+				numlen = 1;
+				*pt = pt[1];
+			}
+			break;
+
+		default:
+			*pt = pt[1];
+		}
+
+		int siz = pt - buff + numlen;
+		pt++;
+		memmove(pt, pt + numlen, len - siz);
+	}
+
+	return buff;
 }
 
 
