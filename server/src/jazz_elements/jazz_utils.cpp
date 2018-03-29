03c14ce23ff509768eb6681b75bf281f88265856
kaalam <kaalam@dybot.com>
2018-03-29 11:52:03 +0200

    Adds the function Murmurhash64A to the utils

--- server/src/jazz_elements/jazz_utils.cpp
+++ server/src/jazz_elements/jazz_utils.cpp
@@ -163,26 +163,20 @@ char *ExpandEscapeSequences(char *buff)
 		case '5':
 		case '6':
 		case '7':
-			if (pt[2] && pt[3] && sscanf(&pt[1], "%3o", &num) == 1 && sscanf(&pt[2], "%3o", &any) == 1 && sscanf(&pt[3], "%3o", &any) == 1)
-			{
+			if (pt[2] && pt[3] && sscanf(&pt[1], "%3o", &num) == 1 && sscanf(&pt[2], "%3o", &any) == 1 && sscanf(&pt[3], "%3o", &any) == 1) {
 				numlen = 3;
 				*pt	 = (char) num;
-			}
-			else
-			{
+			} else {
 				numlen = 1;
 				*pt = pt[1];
 			}
 			break;
 
 		case 'x':
-			if (pt[2] && pt[3] && sscanf(&pt[2], "%2x", &num) == 1 && sscanf(&pt[3], "%2x", &any) == 1)
-			{
+			if (pt[2] && pt[3] && sscanf(&pt[2], "%2x", &num) == 1 && sscanf(&pt[3], "%2x", &any) == 1) {
 				numlen = 3;
 				*pt	 = (char) num;
-			}
-			else
-			{
+			} else {
 				numlen = 1;
 				*pt = pt[1];
 			}
@@ -257,6 +251,63 @@ pid_t FindProcessIdByName(const char *name)
 	closedir(dir);
 
 	return 0;
+}
+
+
+#define MURMUR_SEED	  76493		///< Just a 5 digit prime
+
+/** MurmurHash2, 64-bit versions, by Austin Appleby
+
+	(from https://sites.google.com/site/murmurhash/)
+
+	All code is released to the public domain. For business purposes, Murmurhash is
+	under the MIT license.
+
+	The same caveats as 32-bit MurmurHash2 apply here - beware of alignment
+	and endian-ness issues if used across multiple platforms.
+
+	// 64-bit hash for 64-bit platforms
+*/
+uint64_t MurmurHash64A(const void *key, int len)
+{
+	const uint64_t m = 0xc6a4a7935bd1e995;
+	const int	   r = 47;
+
+	uint64_t h = MURMUR_SEED ^ (len*m);
+
+	const uint64_t *data = (const uint64_t *) key;
+	const uint64_t *end  = data + (len/8);
+
+	while(data != end) {
+		uint64_t k = *data++;
+
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+
+		h ^= k;
+		h *= m;
+	}
+
+	const unsigned char *data2 = (const unsigned char*) data;
+
+	switch(len & 7)	{
+	case 7: h ^= uint64_t(data2[6]) << 48;
+	case 6: h ^= uint64_t(data2[5]) << 40;
+	case 5: h ^= uint64_t(data2[4]) << 32;
+	case 4: h ^= uint64_t(data2[3]) << 24;
+	case 3: h ^= uint64_t(data2[2]) << 16;
+	case 2: h ^= uint64_t(data2[1]) << 8;
+	case 1: h ^= uint64_t(data2[0]);
+
+		h *= m;
+	};
+
+	h ^= h >> r;
+	h *= m;
+	h ^= h >> r;
+
+	return h;
 }
 
 
