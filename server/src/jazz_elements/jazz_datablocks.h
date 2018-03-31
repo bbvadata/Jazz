94d47923274308f4568146e4a70a2128bffffe8a
kaalam <kaalam@dybot.com>
2018-03-31 21:35:00 +0200

    Creates the basic block definition

--- server/src/jazz_elements/jazz_datablocks.h
+++ server/src/jazz_elements/jazz_datablocks.h
@@ -22,6 +22,10 @@
 */
 
 
+#include <chrono>
+#include <limits.h>
+
+
 /**< \brief Basic Jazz codeless data types and constants.
 
 	This module defines the constants and codeless structures to create JazzDataBlock structures.
@@ -43,6 +47,72 @@
 
 namespace jazz_datablocks
 {
+typedef std::chrono::steady_clock::time_point TimePoint;
+
+#define CELL_TYPE__
+
+// 8 bit cell types
+#define CELL_TYPE_BYTE			0x001		///< A tensor of unsigned 8-bit binaries. NA is not defined for this type.
+#define CELL_TYPE_BYTE_BOOLEAN	0x101		///< A tensor 8-bit booleans: 0, 1, JAZZ_BYTE_BOOLEAN_NA = NA.
+
+// 32 bit cell types
+#define CELL_TYPE_INTEGER		0x004		///< A tensor of 32-bit signed integers. NA is JAZZ_INTEGER_NA.
+#define CELL_TYPE_FACTOR		0x104		///< A tensor of 32-bit unsorted categoricals. NA is JAZZ_INTEGER_NA.
+#define CELL_TYPE_GRADE			0x204		///< A tensor of 32-bit sorted categoricals. NA is JAZZ_INTEGER_NA.
+#define CELL_TYPE_BOOLEAN		0x304		///< A tensor of 32-bit booleans: 0, 1, JAZZ_BOOLEAN_NA = NA.
+#define CELL_TYPE_SINGLE		0x404		///< A tensor of IEEE 754 32-bit float (aka single). NA is JAZZ_SINGLE_NA.
+#define CELL_TYPE_JAZZ_STRING	0x504		///< A tensor or 32-bit offsets to unmutable strings or JAZZ_STRING_NA or JAZZ_STRING_EMPTY.
+
+// 64 bit cell types
+#define CELL_TYPE_LONG_INTEGER	0x008		///< A tensor of 64-bit signed integers. NA is JAZZ_LONG_INTEGER_NA.
+#define CELL_TYPE_JAZZ_TIME		0x108		///< A tensor of 64-bit TimePoint. NA is JAZZ_TIME_POINT_NA.
+#define CELL_TYPE_DOUBLE		0x208		///< A vector of floating point numbers. Binary compatible with an R REALSXP (vector of numeric).
+
+// NA values or empty string valuies for all cell_type values.
+#define JAZZ_BYTE_BOOLEAN_NA	0x0ff		///< NA for 8-bit boolean is binary 0xff. Type does not exist in R.
+#define JAZZ_BOOLEAN_NA			0x0ff		///< NA for a 32-bit boolean is binary 0xff. This is R compatible.
+#define JAZZ_INTEGER_NA			INT_MIN		///< NA for a 32-bit integer. This is R compatible.
+#define JAZZ_SINGLE_NA			F_NA		///< NA for a float is the value returned by nanf(). Type does not exist in R.
+#define JAZZ_STRING_NA			0			///< NA for a string coded as CELL_TYPE_JAZZ_STRING
+#define JAZZ_STRING_EMPTY		1			///< An empty string coded as CELL_TYPE_JAZZ_STRING
+#define JAZZ_LONG_INTEGER_NA	LLONG_MIN	///< NA for a 64-bit integer. Type does not exist in R.
+#define JAZZ_TIME_POINT_NA		0			///< NA for a CELL_TYPE_JAZZ_TIME is a 64-bit zero. Type does not exist in R.
+#define JAZZ_DOUBLE_NA			R_NA		///< NA for a double. This is R compatible.
+
+
+/** A block. Anything in Jazz is a block. A block is a JazzBlockHeader, followed by a tensor, then in case
+cell_type == CELL_TYPE_JAZZ_STRING followed by a JazzStringBuffer, and followed by a JazzAttributesMap.
+*/
+struct JazzBlockHeader
+{
+	int jazz_class;				///< The class to which the block belongs. See jazz_classes.h for details.
+	int	cell_type;				///< The type for the cells in the tensor. See CELL_TYPE_*
+	int dim[7];					///< The dimensions of the tensor, all must be zero after the first zero. Size cannot exceed 2 Gb.
+	int total_bytes;			///< Total size of the block everything included.
+	int map_offset;				///< Offset to where the JazzAttributesMap starts.
+	int map_num_elements;		///< Number of elements in the JazzAttributesMap.
+	TimePoint created;			///< Timestamp when the block was created.
+	long long hash64;			///< Hash of everything but the header.
+};
+
+struct JazzStringBuffer
+{
+	char NA, EMPTY;						///< A binary zero making a string with offset 0 (== JAZZC_NA_STRING) or 1 (== JAZZC_EMPTY_STRING) == ""
+	bool isBig;							///< Set by get_string_idx_C_OFFS_CHARS() when the number of strings > STR_SEARCH_BIG_ABOVE
+	int	 last_idx;						///< The index to the first free space after the last stored string
+	char buffer[];						///< The buffer where all the non-empty strings are stored
+};
+
+
+struct JazzAttributesMap
+{
+
+};
+
+
+extern float  F_NA;
+extern double R_NA;
+
 
 }
 
