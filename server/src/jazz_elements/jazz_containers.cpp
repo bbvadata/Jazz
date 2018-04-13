730d4db7abb0d027a2fc9f18b368af61bcd78a10
kaalam <kaalam@dybot.com>
2018-04-13 22:15:00 +0200

    Completes the documentation of the one shot memory allocation functions

--- server/src/jazz_elements/jazz_containers.cpp
+++ server/src/jazz_elements/jazz_containers.cpp
@@ -35,33 +35,93 @@ namespace jazz_containers
 
 /** Create a new (one_shot) JazzBlock as a selection (of possibly all) of an existing JazzBlock
 
-	\param p_as_block   An existing block from which rows can be selected.
-	\param p_row_filter A filter .
+	\param p_as_block   An existing block from which everything is copied, possibly with a selection over its rows.
+	\param p_row_filter A filter that is applicable to p_as_block. I.e., p_row_filter->can_filter(p_as_block) == true
+						If p_row_filter == nullptr then p_as_block is copied into a newly allocated pointer.
+						See parameter dim in the new_jazz_block() version that uses dim to understand how selection is applied.
+	\param att 			An alternative source of attributes. When this parameter in != nullptr, the new block will get its
+						attributes from att instead of copying those in p_as_block->.
+
+	OWNERSHIP: If you create a one shot block using new_jazz_block(), you earn the responsibility to free it with free_jazz_block().
+	This is not the normal way to create JazzBlocks, when you use a JazzBlockKeepr descendant, that object will allocate and free
+	the JazzBlocks automatically. The same applies to JazzBlocks created in the stack of a bebop program which are also managed
+	automatically.
 
 	\return	The new JazzBlock or nullptr if failed. Also logs errors to jazz_containers::log
-
 */
-pJazzBlock new_jazz_block (pJazzBlock  p_as_block,
-						   pJazzFilter p_row_filter)
+pJazzBlock new_jazz_block (pJazzBlock  	  p_as_block,
+						   pJazzFilter 	  p_row_filter,
+						   AllAttributes *att)
 {
 
 }
 
 
-/**
+/** Create a new (one_shot) JazzBlock (including a JazzFilter) from scratch
+
+	\param cell_type		The type for the tensor's cell types in [CELL_TYPE_BYTE..CELL_TYPE_DOUBLE]
+	\param dim 				This defines both the rank and the dimensions of the tensor. Note that, except for the first position a
+							dimension of 0 and 1 is the same dim = {3, 1} is a vector of 3 elements with rank 1, exactly like {3, 0}.
+							As a matter of convention, dim should always end with a 0 except when it is JAZZ_MAX_TENSOR_RANK long.
+							For the first dimension 1 means one element and 0 means no element. Both have rank 1. The latter is the
+							typical result of a selection where no row matches the condition. Blocks never have rank == 0 and zero-element
+							blocks have the same rank as the block from which they were selected. When 0 rows are selected from a block
+							of dim = {r, s, t} the resulting block is {0, s, t} with size == 0 and rank == 3.
+	\param att				The attributes to set when creating the block. They are be inmutable. To change the attributes of a JazzBlock
+							use the version of new_jazz_block() with parameter p_as_block
+	\param fill_tensor 		How to fill the tensor. When creating anything that is not a JazzFilter, p_bool_filter is ignored and the options
+							are: JAZZ_FILL_NEW_DONT_FILL (don't do anything with the tensor), JAZZ_FILL_NEW_WITH_ZERO (fill with binary zero
+							no matter what the cell_type is), JAZZ_FILL_NEW_WITH_NA fill with the appropriate NA for the cell_type)
+							When creating a filter, p_bool_filter must be a vector of length == size and the filter will be created as
+							boolean (when fill_tensor == JAZZ_FILL_BOOLEAN_FILTER) or integer (when fill_tensor == JAZZ_FILL_INTEGER_FILTER)
+	\param p_bool_filter	The vector of boolean (each true value means the corresponding row is selected) used when fill_tensor ==
+							JAZZ_FILL_BOOLEAN_FILTER and fill_tensor == JAZZ_FILL_INTEGER_FILTER
+	\param stringbuff_size 	One of the possible ways to allocate space for strings is declaring this size. When this is non-zero a buffer
+							will be allocated with this size plus whatever size is required by the strings in att. new_jazz_block() will
+							only allocate the space and do nothing with it. The caller should assign strings to cells with JazzBlock.set_string().
+	\param p_text			The other possible way to allocate space for strings is by declaring p_text. Imagine the content of p_text
+							as a text file with n = size rows that will be pushed into the tensor and the string buffer. The eoln character
+							separates the cells.
+	\param eoln		       	A single character that separates the cells in p_text and will not be pushed to the string buffer.
+
+	NOTES: String buffer allocation should not be used to dynamically change attribute values. Attributes are inmutable and should be changed
+	only creating a new block with new = new_jazz_block(p_as_block = old, att = new_att).
+	String buffer allocation should only be used for cell_type == CELL_TYPE_JAZZ_STRING and either with stringbuff_size or with p_text (and eoln).
+	If stringbuff_size is used, JazzBlock.set_string() should be used afterwards. If p_text is used, the tensor is already filled and
+	JazzBlock.set_string() **should not** be called after that.
+
+	OWNERSHIP: If you create a one shot block using new_jazz_block(), you earn the responsibility to free it with free_jazz_block().
+	This is not the normal way to create JazzBlocks, when you use a JazzBlockKeepr descendant, that object will allocate and free
+	the JazzBlocks automatically. The same applies to JazzBlocks created in the stack of a bebop program which are also managed
+	automatically.
+
+	\return	The new JazzBlock or nullptr if failed. Also logs errors to jazz_containers::log
 */
 pJazzBlock new_jazz_block (int			  cell_type,
 						   JazzTensorDim *dim,
 						   AllAttributes *att,
 						   int			  fill_tensor,
-						   void			 *p_data,
+						   bool			 *p_bool_filter,
 						   int			  stringbuff_size,
 						   const char	 *p_text,
-						   char			  separator)
+						   char			  eoln)
 {
 
 }
 
+
+/** Free a (one_shot) JazzBlock created with new_jazz_block()
+
+	\param p_block The pointer to the block you previously created with new_jazz_block().
+
+	OWNERSHIP: Never free a pointer you have not created with new_jazz_block(). The normal way to create JazzBlocks is using a JazzBlockKeepr
+	descendant, that object will allocate and free	the JazzBlocks automatically. Also in the stack of a bebop program JazzBlocks are also
+	managed automatically.
+*/
+void free_jazz_block(pJazzBlock &p_block)
+{
+
+}
 
 /* ----------------------------------------------------------------------------
 
