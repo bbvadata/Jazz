6b356ed8574ac8b60f20e71c2cd3fade11e5a08c
kaalam <kaalam@dybot.com>
2018-05-15 23:37:56 +0200

    Include LMDB

--- server/src/include/jazz.h
+++ server/src/include/jazz.h
@@ -19,6 +19,10 @@
 
    http://www.apache.org/licenses/LICENSE-2.0
 
+   3. LMDB: Copyright 2011-2017 Howard Chu, Symas Corp. All rights reserved.
+
+   Licensed under http://www.OpenLDAP.org/license.html
+
    Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
