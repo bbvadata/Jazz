928d85911431687b715bf72d2b34aeaabcdbbee3
kaalam <kaalam@dybot.com>
2018-05-15 23:14:02 +0200

    Creates httpclient linking dynamically to lib-curl

---
+++ server/src/jazz_elements/jazz_httpclient.h
@@ -0,0 +1,51 @@
+/* Jazz (c) 2018 kaalam.ai (The Authors of Jazz), using (under the same license):
+
+   BBVA - Jazz: A lightweight analytical web server for data-driven applications.
+
+   Copyright 2016-2017 Banco Bilbao Vizcaya Argentaria, S.A.
+
+  This product includes software developed at
+
+   BBVA (https://www.bbva.com/)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+
+/**< \brief Arithmetic, logic and type conversion stdcore applicable to JazzDataBlock structures.
+
+	This module defines pure functions that accept JazzDataBlock structures as arguments and return JazzDataBlock
+structures or throw exceptions. These functions are called "stdcore" for a reason, they are the simplest blocks
+approximately matching Bebop bytecode instructions.
+*/
+
+
+#if defined CATCH_TEST
+#ifndef INCLUDED_JAZZ_CATCH2
+#define INCLUDED_JAZZ_CATCH2
+
+#include "src/catch2/catch.hpp"
+
+#endif
+#endif
+
+
+#ifndef INCLUDED_JAZZ_ELEMENTS_HTTPCLIENT
+#define INCLUDED_JAZZ_ELEMENTS_HTTPCLIENT
+
+namespace jazz_httpclient
+{
+
+}
+
+#endif
