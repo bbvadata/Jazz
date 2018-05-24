8ba01edcc04d9856fd7bfb500a7091dcc72c82ad
kaalam <kaalam@dybot.com>
2018-05-24 18:15:22 +0200

    Adds some PoC code for the http client functionality

--- server/src/jazz_elements/jazz_httpclient.cpp
+++ server/src/jazz_elements/jazz_httpclient.cpp
@@ -22,3 +22,66 @@
 */
 
 #include "src/jazz_elements/jazz_httpclient.h"
+
+
+/** A test write callback.
+
+	(see https://curl.haxx.se/libcurl/c/CURLOPT_WRITEFUNCTION.html)
+*/
+size_t httpclient_callback(char *ptr, size_t size, size_t nmemb, void *p_keepr)
+{
+	size = size*nmemb;
+
+	if (p_keepr == nullptr) {
+//		jCommons.log(LOG_INFO, "write_callback() : Right userdata.");
+	} else {
+//		jCommons.log(LOG_MISS, "write_callback() : Wrong userdata.");
+	}
+
+	printf("\n");
+
+	for (int i = 0; i < size; i++)
+		printf("%c", ptr[i]);
+
+	printf("\n\n");
+
+	return size;
+}
+
+
+/** Any test implemented as a jazzCommons method for convenience.
+
+	\return	true if successful, false and log(LOG_MISS, "further details") if not.
+*/
+bool remote_testing_point ()
+{
+	CURL *curl;
+	CURLcode res;
+
+	curl = curl_easy_init();
+	if (!curl)
+	{
+//		log(LOG_MISS, "jazzCommons::remote_testing_point() : curl_easy_init() failed.");
+
+		return false;
+	}
+
+	curl_easy_setopt(curl, CURLOPT_URL, "http://20.1.71.31:8888///");
+	curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
+	curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, httpclient_callback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) 0xbaaadc0ffee);
+
+	res = curl_easy_perform(curl);
+	if (res != CURLE_OK)
+	{
+//		log(LOG_MISS, "jazzCommons::remote_testing_point() : curl_easy_perform() failed.");
+
+		return false;
+	}
+
+	curl_easy_cleanup(curl);
+
+	return true;
+}
+
