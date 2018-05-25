f0af77fca54aef28c21e6ef0cda8e46519143410
Jacques <jacques@dybot.com>
2018-05-25 22:47:03 +0200

    Fixes issue to prevent dashes becoming options in pyjazz building script

--- py_package/build.sh
+++ py_package/build.sh
@@ -6,14 +6,14 @@ cd pyjazz || return 1
 
 rm -rf dist/
 rm -rf pyjazz.egg-info/
-rm -f *.rst
+rm -f ./*.rst
 
 cp ../html/index.md README.md
 pandoc --from=markdown --to=rst --output=README.rst README.md
 
 cd pyjazz || return 1
 
-rm -f *.so
+rm -f ./*.so
 
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
@@ -23,13 +23,13 @@ g++ -I../../../server -I/usr/include -DNDEBUG -c -o jazz_miscutils.o ../../../se
 g++ -c -fpic jazz_blocks_wrap.c -I/usr/include/python2.7
 g++ -shared jazz_miscutils.o jazz_blocks_wrap.o -o _jazz_blocks2.so
 
-rm *.o
+rm ./*.o
 
 g++ -I../../../server -I/usr/include -DNDEBUG -c -o jazz_miscutils.o ../../../server/src/jazz_utils/jazz_miscutils.cpp
 g++ -c -fpic jazz_blocks_wrap.c -I/usr/include/python3.6
 g++ -shared jazz_miscutils.o jazz_blocks_wrap.o -o _jazz_blocks3.so
 
-rm *.o
+rm ./*.o
 
 rm jazz_blocks_wrap.c
 
