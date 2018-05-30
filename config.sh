a76cf8b74abc7aa4c13d0229586d9b034811ea7f
Jacques <jacques@dybot.com>
2018-05-30 12:56:21 +0200

    Fixes encoding

--- config.sh
+++ config.sh
@@ -227,7 +227,7 @@ printf "Ok.\n"
 printf "Writing: py_package/pyjazz/pyjazz/get_jazz_version.py ... "
 
 echo "def get_jazz_version():
-	return(\"$jazz_version\")" > py_package/pyjazz/pyjazz/get_jazz_version.py
+    return(\"$jazz_version\")" > py_package/pyjazz/pyjazz/get_jazz_version.py
 
 printf "Ok.\n"
 
@@ -236,42 +236,42 @@ printf "Writing: py_package/pyjazz/setup.py ... "
 printf "from setuptools import setup
 
 try:
-	with open('README.md', 'r') as ldf:
-		long_desc = ldf.read()
+    with open('README.md', 'r') as ldf:
+        long_desc = ldf.read()
 except FileNotFoundError:
-	with open('README.rst', 'r') as ldf:
-		long_desc = ldf.read().replace('\n', '')
+    with open('README.rst', 'r') as ldf:
+        long_desc = ldf.read().replace('\n', '')
 
 setup(name='pyjazz',
-	version					= %s,
-	description				= 'Official Python client for Jazz',
-	long_description		= long_desc,
-	url						= 'http://github.com/kaalam/jazz',
-	author					= 'kaalam.ai',
-	author_email			= 'kaalam@kaalam.ai',
-	license					= 'Apache 2.0',
-	packages				= ['pyjazz'],
-	package_data			= {'pyjazz': ['_jazz_blocks2.so', '_jazz_blocks3.so']},
-	include_package_data	= True,
-	zip_safe				= False,
-	classifiers			    = [
-		'Development Status :: 2 - Pre-Alpha',
-		'Intended Audience :: Developers',
-		'Intended Audience :: Education',
-		'Intended Audience :: Financial and Insurance Industry',
-		'Intended Audience :: Healthcare Industry',
-		'Intended Audience :: Information Technology',
-		'Intended Audience :: Manufacturing',
-		'Intended Audience :: Science/Research',
-		'Intended Audience :: System Administrators',
-		'Intended Audience :: Telecommunications Industry',
-		'License :: OSI Approved :: Apache Software License',
-		'Operating System :: POSIX :: Linux',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    version                 = %s,
+    description             = 'Official Python client for Jazz',
+    long_description        = long_desc,
+    url                     = 'http://github.com/kaalam/jazz',
+    author                  = 'kaalam.ai',
+    author_email            = 'kaalam@kaalam.ai',
+    license                 = 'Apache 2.0',
+    packages                = ['pyjazz'],
+    package_data            = {'pyjazz': ['_jazz_blocks2.so', '_jazz_blocks3.so']},
+    include_package_data    = True,
+    zip_safe                = False,
+    classifiers             = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Manufacturing',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
       ],
-	)" "$jazz_version" > py_package/pyjazz/setup.py
+    )" "$jazz_version" > py_package/pyjazz/setup.py
 
 printf "Ok.\n"
 
