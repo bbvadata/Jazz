a04395b8fb7153a525eac1e46fe219e3eb55daa0
Jacques <jacques@dybot.com>
2018-05-31 13:45:27 +0200

    Refactors the short_name as a variable to avoid duplicates

--- config.sh
+++ config.sh
@@ -97,7 +97,8 @@ echo "$2   iterating in dep($dp)" >&2
     if [ -e "$dp" ]; then
 echo "$2     file exists" >&2
       # shellcheck disable=SC2001
-      recursive_parse_header_result="$recursive_parse_header_result $(echo "$dp" | sed 's/.*\(jazz.*h\|test_.*ctest\).*/\1/')"
+	  short_name=$(echo "$dp" | sed 's/.*\(jazz.*h\|test_.*ctest\).*/\1/')
+      recursive_parse_header_result="$recursive_parse_header_result $short_name"
 
 	  recursive_parse_header_result=$(recursive_parse_header "$dp" "$2 _")
     fi
