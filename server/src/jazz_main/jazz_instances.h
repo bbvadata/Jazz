a1306b688c26a156d295f113dc45b1ce85c23f7b
Jacques <jacques@dybot.com>
2018-06-03 13:40:30 +0000

    Creates a skeleton header for jazz_instances

--- server/src/jazz_main/jazz_instances.h
+++ server/src/jazz_main/jazz_instances.h
@@ -21,3 +21,23 @@
   limitations under the License.
 */
 
+#include "src/jazz_main/jazz_restapi.h"
+
+
+#ifndef INCLUDED_JAZZ_MAIN_INSTANCES
+#define INCLUDED_JAZZ_MAIN_INSTANCES
+
+/**< \brief TODO
+
+//TODO: Write module description for jazz_instances when implemented.
+*/
+namespace jazz_instances
+{
+
+//TODO: Document interface for module jazz_instances.
+
+//TODO: Implement interface for module jazz_instances.
+
+}
+
+#endif
