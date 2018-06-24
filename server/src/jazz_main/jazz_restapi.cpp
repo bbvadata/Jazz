29330bf6d14204e166326151eae22165e41b9373
Jacques <jacques@dybot.com>
2018-06-24 16:52:34 +0200

    Includes namespace

--- server/src/jazz_main/jazz_restapi.cpp
+++ server/src/jazz_main/jazz_restapi.cpp
@@ -26,6 +26,9 @@
 namespace jazz_restapi
 {
 
+using namespace std;
+
+
 /** The server's MHD_Daemon created by MHD_start_daemon() and needed for MHD_stop_daemon()
 */
 struct MHD_Daemon *Jazz_MHD_Daemon;
