2b305cf56ace1007bb2c765f100132a23fbd2c18
Jacques <jacques@dybot.com>
2018-06-29 20:07:15 +0200

    Implements a minimalist ShutDown for JazzCluster

--- server/src/jazz_functional/jazz_cluster.cpp
+++ server/src/jazz_functional/jazz_cluster.cpp
@@ -54,6 +54,19 @@ JazzCluster::~JazzCluster()
 //TODO: Implement ~JazzCluster
 }
 
+
+/** Close the JazzCluster object persisting pending cached write operations, freeing resources, etc.
+
+	\param restarting_service Tell the object that it will be used again immediately if true and that makes any difference..
+	\return					  JAZZ_API_NO_ERROR or any other API_ErrorCode in cases errors occurred. (Errors will also be logged out.)
+*/
+API_ErrorCode JazzCluster::ShutDown (bool restarting_service)
+{
+	log(LOG_INFO, "Completed JazzCluster::ShutDown()");
+
+	return JAZZ_API_NO_ERROR;
+}
+
 } // namespace jazz_cluster
 
 
