4cb0b8309a4220989ca2aaa14ef7b6f972e3cf8e
kaalam <kaalam@kaalam.ai>
2018-08-28 19:12:40 +0000

    Cleans up TODO messages for clarity and message length

--- server/src/jazz_functional/jazz_bebop.h
+++ server/src/jazz_functional/jazz_bebop.h
@@ -24,39 +24,34 @@
 /* NOTES from the Jazz draft description including tasks for this
    --------------------------------------------------------------
 
-//TODO: BopCompiler Bebop compiles into bytecode automatically.
+//TODO: interface, implement, code, test:  Bebop (see notes)
 
-//TODO: A Bebop function is a block of compiled bytecode with its source code stored as an attribute.
+//NOTES:
 
-//TODO: Bebop is functional (== functions accept functions as arguments and can return functions), blocks are immutable, reads are safe,
-		writes are idempotent, side effects are not possible.
-
-//TODO: Bebop source code uses operators (both in the standard core or defined in Bebop) to support “natural expressions”. Operators are
-		only syntactic sugar for functions.
-
-//TODO: Function overloading is possible with and order and a match for type checking. This provides some metaprogramming, like defining
-		math over many types in one declaration. This also applies to operators (since they are just functions).
-
-//TODO: Bebop is object oriented. Bebop supports multiple inheritance using combined classes of any number of parents.
-//TODO: Objects can be forked from other objects. A forked object points to its ancestor and is initialized with its state without using
-		extra storing space (until its state changes from that of its ancestor).
-//TODO: A class is internally a keepr that contains variables and functions.
-
-//TODO: Errors in a wrong mutation can be handled by the ancestor function. Bebop supports alternative functions for managing errors.
-
-//TODO: Automatic function result caching. Use of "memoize" directive. (Or memoize by default and declare "prevent_result_cache")
-
-//TODO: Consider 'using ... as ...' the easily create aliases of lvalues and rvalues.
-//TODO: Consider 'matching ... as {... {} ... {} ... {}}' as the case syntax. (Bebop supports match for switching (as in Rust).)
-//TODO: Consider a logic for type assertion (possibly with regex) A bebop function has a low-level way to specify the first, 2nd, ... block
-		passed as arguments. It may also put restriction on types (any numeric sorted, etc.) that throw exceptions when not met. Since
-		refering to the argument as $1, $2, ... is unfriendly, the argument declaration implicitly creates a series of 'using $1 as
-		latitude' statement that allow using the argument with a nice name and an assertion on its type.
-
-//TODO: Program flow level 1 - 'for each ... {}'
-//TODO: Program flow level 2 - 'if ... {}', 'if not ... {}', 'if ... {} else {}'
-//TODO: Program flow level 3 - 'while ... {}', 'do {} while ...'
-//TODO: Program flow level 4 - 'break', 'fail ...', 'return ...'
+  * BopCompiler Bebop compiles into bytecode automatically.
+  * A Bebop function is a block of compiled bytecode with its source code stored as an attribute.
+  * Bebop is functional (== functions accept functions as arguments and can return functions), blocks are immutable, reads are safe,
+	writes are idempotent, side effects are not possible.
+  * Bebop source code uses operators (both in the standard core or defined in Bebop) to support “natural expressions”. Operators are
+	only syntactic sugar for functions.
+  * Function overloading is possible with and order and a match for type checking. This provides some metaprogramming, like defining
+	math over many types in one declaration. This also applies to operators (since they are just functions).
+  * Bebop is object oriented. Bebop supports multiple inheritance using combined classes of any number of parents.
+  * Objects can be forked from other objects. A forked object points to its ancestor and is initialized with its state without using
+	extra storing space (until its state changes from that of its ancestor).
+  * A class is internally a keepr that contains variables and functions.
+  * Errors in a wrong mutation can be handled by the ancestor function. Bebop supports alternative functions for managing errors.
+  * Automatic function result caching. Use of "memoize" directive. (Or memoize by default and declare "prevent_result_cache")
+  * Consider 'using ... as ...' the easily create aliases of lvalues and rvalues.
+  * Consider 'matching ... as {... {} ... {} ... {}}' as the case syntax. (Bebop supports match for switching (as in Rust).)
+  * Consider a logic for type assertion (possibly with regex) A bebop function has a low-level way to specify the first, 2nd, ... block
+	passed as arguments. It may also put restriction on types (any numeric sorted, etc.) that throw exceptions when not met. Since
+	refering to the argument as $1, $2, ... is unfriendly, the argument declaration implicitly creates a series of 'using $1 as
+	latitude' statement that allow using the argument with a nice name and an assertion on its type.
+  * Program flow level 1 - 'for each ... {}'
+  * Program flow level 2 - 'if ... {}', 'if not ... {}', 'if ... {} else {}'
+  * Program flow level 3 - 'while ... {}', 'do {} while ...'
+  * Program flow level 4 - 'break', 'fail ...', 'return ...'
 
 */
 
