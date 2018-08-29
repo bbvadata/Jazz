c68991005e84c7b0462e595d42caff87f9139633
kaalam <kaalam@kaalam.ai>
2018-08-29 20:51:09 +0000

    Instruments debugging functionality for the http callback function

--- server/src/jazz_main/jazz_restapi.cpp
+++ server/src/jazz_main/jazz_restapi.cpp
@@ -30,6 +30,20 @@ namespace jazz_restapi
 
 using namespace std;
 
+#ifdef DEBUG
+jazz_utils::JazzLogger	httpLog("http_log");
+
+int print_out_key (void *cls, enum MHD_ValueKind kind,
+				   const char *key, const char *value)
+{
+	httpLog.log_printf(LOG_DEBUG, "| HTTP callback - conn (key:value) : %s:%.40s", key, value);
+
+	return MHD_YES;
+}
+#endif
+
+
+
 //TODO: Implement module jazz_restapi.
 
 /**
@@ -44,8 +58,21 @@ int JazzCallbackAnswerHTTP(void *cls,
 						   size_t *upload_data_size,
 						   void **con_cls)
 {
-/*
-	// Step 1: First opportunity to end the connection before uploading or getting. Not used. We initialize con_cls for the next call.
+#ifdef DEBUG
+	httpLog.log_printf(LOG_DEBUG, "+----------------------------------+----------------------------+");
+	httpLog.log_printf(LOG_DEBUG, "| HTTP callback - cls \x20 \x20 \x20 \x20 \x20 \x20 \x20: %p", cls);
+	httpLog.log_printf(LOG_DEBUG, "| HTTP callback - connection \x20 \x20 \x20 : %p", connection);
+	MHD_get_connection_values (connection, MHD_HEADER_KIND, &print_out_key, NULL);
+	httpLog.log_printf(LOG_DEBUG, "| HTTP callback - url \x20 \x20 \x20 \x20 \x20 \x20 \x20: %s", url);
+	httpLog.log_printf(LOG_DEBUG, "| HTTP callback - method \x20 \x20 \x20 \x20 \x20 : %s", method);
+	httpLog.log_printf(LOG_DEBUG, "| HTTP callback - version \x20 \x20 \x20 \x20 \x20: %s", version);
+	httpLog.log_printf(LOG_DEBUG, "| HTTP callback - upload_data \x20 \x20 \x20: %p", upload_data);
+	httpLog.log_printf(LOG_DEBUG, "| HTTP callback - upload_data_size : %d", *upload_data_size);
+	httpLog.log_printf(LOG_DEBUG, "| HTTP callback - *con_cls \x20 \x20 \x20 \x20 : %p", *con_cls);
+	httpLog.log_printf(LOG_DEBUG, "+----------------------------------+----------------------------+");
+#endif
+
+/*	// Step 1: First opportunity to end the connection before uploading or getting. Not used. We initialize con_cls for the next call.
 
 	if (*con_cls == NULL)
 	{
@@ -56,8 +83,7 @@ int JazzCallbackAnswerHTTP(void *cls,
 
 	// Step 2 : Continue uploads in progress, checking all possible error conditions.
 
-	int i_method;
-	i_method = i_methods[jazz_utils::TenBitsAtAddress(method)];
+	int i_method = i_methods[jazz_utils::TenBitsAtAddress(method)];
 
 	parsedURL pars;
 
@@ -120,22 +146,6 @@ int JazzCallbackAnswerHTTP(void *cls,
 	// Step 4 : This point is reached just once per http petition. Parse the query, returns errors and web pages, continue to API.
 
 	struct MHD_Response * response;
-
-#ifdef DEBUG
-	jCommons.log_printf(LOG_DEBUG, "+----------------------------------+----------------------------+");
-	jCommons.log_printf(LOG_DEBUG, "| HTTP callback - cls \x20 \x20 \x20 \x20 \x20 \x20 \x20: %p", cls);
-	jCommons.log_printf(LOG_DEBUG, "| HTTP callback - connection \x20 \x20 \x20 : %p", connection);
-	MHD_get_connection_values (connection, MHD_HEADER_KIND, &print_out_key, NULL);
-	jCommons.log_printf(LOG_DEBUG, "| HTTP callback - url \x20 \x20 \x20 \x20 \x20 \x20 \x20: %s", url);
-	jCommons.log_printf(LOG_DEBUG, "| HTTP callback - method \x20 \x20 \x20 \x20 \x20 : %s", method);
-	jCommons.log_printf(LOG_DEBUG, "| HTTP callback - version \x20 \x20 \x20 \x20 \x20: %s", version);
-	jCommons.log_printf(LOG_DEBUG, "| HTTP callback - upload_data \x20 \x20 \x20: %p", upload_data);
-	jCommons.log_printf(LOG_DEBUG, "| HTTP callback - upload_data_size : %d", *upload_data_size);
-	jCommons.log_printf(LOG_DEBUG, "| HTTP callback - *con_cls \x20 \x20 \x20 \x20 : %p", *con_cls);
-	jCommons.log_printf(LOG_DEBUG, "+----------------------------------+----------------------------+");
-
-	response = NULL;	// clang warning
-#endif
 
 	switch (i_method)
 	{
@@ -885,15 +895,6 @@ int	   i_methods[1024];
 ------------------------------------------------- */
 
 #define MHD_HTTP_ANYERROR 400
-
-#ifdef DEBUG
-int print_out_key (void *cls, enum MHD_ValueKind kind,
-				   const char *key, const char *value)
-{
-	jCommons.log_printf(LOG_DEBUG, "| HTTP callback - conn (key:value) : %s:%.40s", key, value);
-	return MHD_YES;
-}
-#endif
 
 
 /// Pointers to these global variables control the state of a PUT call.
