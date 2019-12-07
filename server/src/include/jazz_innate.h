c8d52369aca3d4985836e28ed2a5800671f76262
kaalam <kaalam@kaalam.ai>
2019-12-07 21:34:08 +0100

    Adds a new application scaffold

---
+++ server/src/include/jazz_innate.h
@@ -0,0 +1,55 @@
+/* Jazz (c) 2018-2019 kaalam.ai (The Authors of Jazz), using (under the same license):
+
+   1. Biomodelling - The AATBlockQueue class (c) Jacques Basaldúa, 2009-2012 licensed
+      exclusively for the use in the Jazz server software.
+
+      Copyright 2009-2012 Jacques Basaldúa
+
+   2. BBVA - Jazz: A lightweight analytical web server for data-driven applications.
+
+      Copyright 2016-2017 Banco Bilbao Vizcaya Argentaria, S.A.
+
+      This product includes software developed at
+
+      BBVA (https://www.bbva.com/)
+
+   3. LMDB, Copyright 2011-2017 Howard Chu, Symas Corp. All rights reserved.
+
+   Licensed under http://www.OpenLDAP.org/license.html
+
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+
+#include "src/include/jazz_elements.h"
+
+
+#ifndef INCLUDED_JAZZ_INNATE
+#define INCLUDED_JAZZ_INNATE
+
+
+/**< \brief One liner.
+
+//TODO: Write this!
+*/
+
+#include "src/jazz_innate/root_fields.h"
+#include "src/jazz_innate/std_math.h"
+#include "src/jazz_innate/std_algebra.h"
+#include "src/jazz_innate/std_logic.h"
+#include "src/jazz_innate/speech.h"
+#include "src/jazz_innate/english.h"
+
+
+#endif // ifndef INCLUDED_JAZZ_INNATE
