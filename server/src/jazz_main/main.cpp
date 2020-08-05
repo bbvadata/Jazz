7d2af922501e6e05a9819309fad443c51ae8044e
kaalam <kaalam@kaalam.ai>
2020-08-05 15:52:39 +0200

    Edits main in new scaffold

--- server/src/jazz_main/main.cpp
+++ server/src/jazz_main/main.cpp
@@ -1,11 +1,11 @@
 /* Jazz (c) 2018-2020 kaalam.ai (The Authors of Jazz), using (under the same license):
 
-   1. Biomodelling - The AATBlockQueue class (c) Jacques Basaldúa, 2009-2012 licensed
-      exclusively for the use in the Jazz server software.
+	1. Biomodelling - The AATBlockQueue class (c) Jacques Basaldúa, 2009-2012 licensed
+	  exclusively for the use in the Jazz server software.
 
-      Copyright 2009-2012 Jacques Basaldúa
+	  Copyright 2009-2012 Jacques Basaldúa
 
-   2. BBVA - Jazz: A lightweight analytical web server for data-driven applications.
+	2. BBVA - Jazz: A lightweight analytical web server for data-driven applications.
 
       Copyright 2016-2017 Banco Bilbao Vizcaya Argentaria, S.A.
 
@@ -13,26 +13,24 @@
 
       BBVA (https://www.bbva.com/)
 
-   3. LMDB, Copyright 2011-2017 Howard Chu, Symas Corp. All rights reserved.
+	3. LMDB, Copyright 2011-2017 Howard Chu, Symas Corp. All rights reserved.
 
-   Licensed under http://www.OpenLDAP.org/license.html
+	  Licensed under http://www.OpenLDAP.org/license.html
 
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+	  Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0
+	  http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
+	  Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
 */
 
-
-// #include <stl_whatever>
 
 #define CATCH_CONFIG_MAIN		//This tells Catch2 to provide a main() - has no effect when CATCH_TEST is not defined
 
--- server/src/jazz_main/main.h
+++ server/src/jazz_main/main.h
@@ -1,11 +1,11 @@
 /* Jazz (c) 2018-2020 kaalam.ai (The Authors of Jazz), using (under the same license):
 
-   1. Biomodelling - The AATBlockQueue class (c) Jacques Basaldúa, 2009-2012 licensed
-      exclusively for the use in the Jazz server software.
+	1. Biomodelling - The AATBlockQueue class (c) Jacques Basaldúa, 2009-2012 licensed
+	  exclusively for the use in the Jazz server software.
 
-      Copyright 2009-2012 Jacques Basaldúa
+	  Copyright 2009-2012 Jacques Basaldúa
 
-   2. BBVA - Jazz: A lightweight analytical web server for data-driven applications.
+	2. BBVA - Jazz: A lightweight analytical web server for data-driven applications.
 
       Copyright 2016-2017 Banco Bilbao Vizcaya Argentaria, S.A.
 
@@ -13,28 +13,30 @@
 
       BBVA (https://www.bbva.com/)
 
-   3. LMDB, Copyright 2011-2017 Howard Chu, Symas Corp. All rights reserved.
+	3. LMDB, Copyright 2011-2017 Howard Chu, Symas Corp. All rights reserved.
 
-   Licensed under http://www.OpenLDAP.org/license.html
+	  Licensed under http://www.OpenLDAP.org/license.html
 
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+	  Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0
+	  http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
+	  Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
 */
 
 
-// #include <stl_whatever>
+#include <iostream>
+#include <signal.h>
 
-// #include "src/jazz_main/xxx.h"
+
+#include "src/jazz_main/instances.h"
 
 #if defined CATCH_TEST
 #ifndef INCLUDED_JAZZ_CATCH2
@@ -56,12 +58,6 @@
 	The entry point is replaced by the catch2 entry point for unit testing.
 */
 
-
-#include <iostream>
-#include <signal.h>
-
-
-#include "src/jazz_main/instances.h"
 
 // Command line arguments for the Jazz server
 
