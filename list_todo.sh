12c0ca54c45c111ffeb8c1abf9559e52db141a42
kaalam <kaalam@kaalam.ai>
2021-01-01 10:54:26 +0100

    [AUTOGEN] Happy New Year!

--- list_todo.sh
+++ list_todo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#    (c) 2018-2020 kaalam.ai (The Authors of Jazz)
+#    (c) 2018-2021 kaalam.ai (The Authors of Jazz)
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
