7f45a8722291313c7ec9432759f6d51df26369fd
kaalam <kaalam@kaalam.ai>
2021-07-12 18:08:04 +0200

    Adds interface type

--- server/src/jazz_elements/tuple.h
+++ server/src/jazz_elements/tuple.h
@@ -54,9 +54,9 @@
 namespace jazz_elements
 {
 
-/** \brief Tuple: A Jazz data block of a Kind.
+/// An array of pointers to Blocks to create Tuples in one call
+typedef pBlock Blocks[0];
 
-An instance of a **Kind**. Physically, like a Kind, it is a single block with some differences:
 
 - It holds data and metadata. (It has a reference to the kind, but that is only interesting in the context of formal fields. Anything
   required to understand the data architecture is in the block itself.)
