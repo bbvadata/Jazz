6d13cb8ba7fa4bcda864645a22d0263961e7b2f9
kaalam <kaalam@kaalam.ai>
2021-08-04 20:55:35 +0200

    Changes get_item_name() interface keeping compatibility

--- server/src/jazz_elements/container.h
+++ server/src/jazz_elements/container.h
@@ -474,11 +474,12 @@ class Container : public Service {
 			\param p_in			The input char stream cursor.
 			\param num_bytes	The number of bytes with data above *p_in
 			\param p_out		The Name buffer that gets the answer if no error was found
+			\param check_quotes	Expects the name to be quoted (like item names) or not (like dimensions)
+			\param check_colon	Expects the name to be followed by a colon (like item names).
 
 			\return	True on success
 		*/
-		inline bool get_item_name(pChar &p_in, int &num_bytes, pChar p_out) {
-			if (skip_space(p_in, num_bytes) <= 0)
+		inline bool get_item_name(pChar &p_in, int &num_bytes, pChar p_out, bool check_quotes = true, bool check_colon = true) {
 				return false;
 
 			if (get_char(p_in, num_bytes) != '"')
