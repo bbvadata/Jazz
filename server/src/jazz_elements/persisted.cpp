032a299c9ae38e0776b4af38c7d3fc675804991d
kaalam <kaalam@kaalam.ai>
2021-08-17 00:09:12 +0200

    Documents and implements get() (form 3)

--- server/src/jazz_elements/persisted.cpp
+++ server/src/jazz_elements/persisted.cpp
@@ -282,20 +282,38 @@ StatusCode Persisted::get (pTransaction &p_txn, Locator &what, pBlock p_row_filt
 }
 
 
+/** Native (Persistence) interface **selection of a tensor in a Tuple** retrieval.
+
+	\param p_txn	A pointer to a Transaction passed by reference. If successful, the Container will return a pointer to a
+					Transaction inside the Container.
+	\param what		Some Locator to the block. E.g. //lmdb/entity/key
+	\param name		The name of the item to be selected.
+
+	\return	SERVICE_NO_ERROR on success (and a valid p_txn), or some negative value (error).
+
+Usage-wise, this is equivalent to a new_block() call. On success, it will return a Transaction that belongs to the Container and must
+be destroy()-ed when the caller is done.
 */
 StatusCode Persisted::get (pTransaction &p_txn, Locator &what, pChar name) {
 
-//TODO: Implement this.
+	pMDB_txn p_l_txn;
 
-	if (!check_block(p_txn->p_block))
-		log_printf(LOG_WARN, "hash64 check failed for //%s/%s/%s", what.base, what.entity, what.key);
+	pBlock p_blx = lock_pointer_to_block(what, p_l_txn);
 
-	return SERVICE_NOT_IMPLEMENTED;		// API Only: One-shot container does not support this.
+	if (p_blx == nullptr) {
+		p_txn = nullptr;
+
+		return SERVICE_ERROR_BLOCK_NOT_FOUND;
+	}
+
+	StatusCode ret = new_block(p_txn, p_blx, name);
+
+	done_pointer_to_block(p_l_txn);
+
+	return ret;
 }
 
 
-/**
-//TODO: Document this.
 */
 StatusCode Persisted::header (StaticBlockHeader &p_txn, Locator &what) {
 
