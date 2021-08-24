7033ed794b1111fee0d946c498a6ee944f455d38
kaalam <kaalam@kaalam.ai>
2021-08-24 23:26:45 +0200

    Defines HttpQueryState apply values

--- server/src/jazz_main/api.h
+++ server/src/jazz_main/api.h
@@ -79,6 +79,21 @@ using namespace jazz_bebop;
 using namespace jazz_agency;
 
 
+/// HttpQueryState apply values (on state == PSTATE_COMPLETE_OK)
+
+#define APPLY_NOTHING					 0		///< Just an r_value with {///node}//base/entity or {///node}//base/entity/key
+#define APPLY_NAME						 1		///< //base/entity/key:name (Select an item form a Tuple by name)
+#define APPLY_URL						 2		///< {///node}//base# any_url_encoded_url ; (A call to http or file)
+#define APPLY_FUNCTION					 3		///< {///node}//base/entity/key(//r_base/r_entity/r_key) (A function call on a block.)
+#define APPLY_FUNCT_CONST				 4		///< {///node}//base/entity/key(# any_url_encoded_const ;) (A function call on a const.)
+#define APPLY_FILTER					 5		///< {///node}//base/entity/key[//r_base/r_entity/r_key] (A filter on a block.)
+#define APPLY_FILT_CONST				 6		///< {///node}//base/entity/key[# any_url_encoded_const ;] (A filter on a const.)
+#define APPLY_RAW						 7		///< {///node}//base/entity/key.raw (Serialize text to raw.)
+#define APPLY_TEXT						 8		///< {///node}//base/entity/key.text (Serialize raw to text.)
+#define APPLY_ASSIGN					 9		///< {///node}//base/entity/key=//r_base/r_entity/r_key (Assign block to block.)
+#define APPLY_ASSIGN_CONST				10		///< {///node}//base/entity/key=# any_url_encoded_const ; (Assign const to block.)
+
+
 /** \brief A buffer to keep the state while parsing/executing a query
 */
 struct HttpQueryState {
