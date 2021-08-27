2312e913b2cf9759aee71d28834980c1bde202c4
kaalam <kaalam@kaalam.ai>
2021-08-27 20:27:49 +0200

    Forks TenBitsLUT into two types, one for int, one for pointers

--- server/src/jazz_elements/utils.h
+++ server/src/jazz_elements/utils.h
@@ -129,8 +129,12 @@ uint64_t	 MurmurHash64A		   (const void *key, int len);
 std::string	 CleanConfigArgument   (std::string s);
 
 
-/// A lookup table for all the possible results of a TenBitsAtAddress() call.
-typedef int TenBitsLUT[TENBITS_LUT_SIZE];
+/// A lookup table for all the possible results of a TenBitsAtAddress() call -> integer.
+typedef int TenBitIntLUT[TENBITS_LUT_SIZE];
+
+
+/// A lookup table for all the possible results of a TenBitsAtAddress() call -> pointer.
+typedef void* TenBitPtrLUT[TENBITS_LUT_SIZE];
 
 
 /** \brief Get ten bits taking the least significant 5 of the first two characters of a string.
