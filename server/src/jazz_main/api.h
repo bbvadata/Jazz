fc8dcd9c9f0a229075128d0f9602dd872011026b
kaalam <kaalam@kaalam.ai>
2021-08-27 18:28:42 +0200

    Simplifies API types

--- server/src/jazz_main/api.h
+++ server/src/jazz_main/api.h
@@ -128,7 +128,7 @@ struct HttpQueryState {
 
 
 typedef struct MHD_Response *pMHD_Response;
-
+typedef struct MHD_Connection *pMHD_Connection;
 
 // ConfigFile and Logger shared by all services
 
@@ -169,29 +169,29 @@ class Api : public Container {
 
 		// parsing methods
 
-		bool parse						(HttpQueryState			&q_state,
-										 pChar					 p_url,
-										 int					 method);
+		bool parse						(HttpQueryState	&q_state,
+										 pChar			 p_url,
+										 int			 method);
 
-		MHD_StatusCode get_static		(pMHD_Response			&response,
-										 pChar					 p_url,
-										 bool					 get_it = true);
+		MHD_StatusCode get_static		(pMHD_Response	&response,
+										 pChar			 p_url,
+										 bool			 get_it = true);
 
 		// deliver http error pages
 
-		MHD_Result return_error_message (struct MHD_Connection	*connection,
-										 const char				*url,
-										 int					 http_status);
+		MHD_Result return_error_message (pMHD_Connection connection,
+										 pChar			 p_url,
+										 int			 http_status);
 
 		// Specific execution methods
 
-		MHD_StatusCode http_put			(const char				*p_upload,
-										 size_t					 size,
-										 HttpQueryState			&q_state,
-										 bool					 continue_upload);
-		MHD_StatusCode http_delete		(HttpQueryState			&q_state);
-		MHD_StatusCode http_get			(pMHD_Response			&response,
-										 HttpQueryState			&q_state);
+		MHD_StatusCode http_put			(pChar			 p_upload,
+										 size_t			 size,
+										 HttpQueryState	&q_state,
+										 bool			 continue_upload);
+		MHD_StatusCode http_delete		(HttpQueryState	&q_state);
+		MHD_StatusCode http_get			(pMHD_Response	&response,
+										 HttpQueryState	&q_state);
 
 #ifndef CATCH_TEST
 	private:
