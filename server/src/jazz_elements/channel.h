c19c6132962ed6df8f6d02178f0004e5497dac40
kaalam <kaalam@kaalam.ai>
2021-09-26 13:25:51 +0200

    Updates "0-mq" Reference

--- server/src/jazz_elements/channel.h
+++ server/src/jazz_elements/channel.h
@@ -121,14 +121,16 @@ stored by some remote server.
 
 The 0-mq implementation in jazz_elements is only a zeroMQ client, not a server. The only operation it supports is a translate() call.
 For a translate() call to succeed, you must first have created a pipeline using a put call with a string. E.g,
-put("//0-mq/pipeline/speech2text") with a block containing the string "tcp://localhost:5555" creates a pipeline named speech2text.
-Outside Jazz, you set up a zeroMQ server that does speech to text, expecting, say 32-bit int vectors as input and returning a buffer of
-char as output. In your translate call you must provide a tuple with two Tensors with item names "input" and "result". The translate()
-call will send the raw tensor of the item "input" to the server and write whatever the server answers into the tensor named "result".
-This operation expects the tensor to be binary (i.e., no variable length strings) and their shapes and types known by the caller.
-In terms of the Jazz server API, this is a function call: either get("//0-mq/pipeline/speech2text(//lmdb/stuff/my_tensor)") or
-get("//0-mq/pipeline/speech2text(#[1,2,3];)") the argument can be anything in Persisted, Volatile, even a file or an //http get or
-a (%-encoded) constant as in the second case.
+put("//0-mq/pipeline/speech2text") with a block containing a string like "tcp://localhost:5555" creates a pipeline named speech2text.
+Outside Jazz, you set up a zeroMQ server that listens to tcp://localhost:5555 and does speech to text, expecting, say 32-bit int vectors
+as input and returning a buffer of char as output. In your translate call you must provide a tuple with two Tensors with item names "input"
+and "result". The translate() call will send the raw tensor of the item "input" to the server and write whatever the server answers into
+the tensor named "result", just overriding the tensor without any dimension change.
+This operation expects the tensor to be binary (i.e., no variable length strings) and their shapes and types known by both parts.
+In terms of the Jazz server API, this is a function call: either GET "//0-mq/pipeline/speech2text(//lmdb/stuff/my_tensor)" or
+GET "//0-mq/pipeline/speech2text(#[1,2,3];)" the argument can be anything in Persisted, Volatile, even a file or an //http get or
+a (%-encoded) constant as in the second example.
+When using translate() as the method of Channel, you should omit the "pipeline" part, just translate(p_tuple, "//0-mq/speech2text");
 
 Besides this, get("//0-mq/pipeline/speech2text") will return just a block with "tcp://localhost:5555" and
 remove("//0-mq/pipeline/speech2text") will destroy the pipeline. Any other call using "0-mq" returns SERVICE_ERROR_NOT_APPLICABLE.
