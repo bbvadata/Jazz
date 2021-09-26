e7822835501331948d83f5af418b63562d447daf
kaalam <kaalam@kaalam.ai>
2021-09-26 10:44:10 +0200

    Removes unnecessary note

--- server/src/jazz_elements/channel.h
+++ server/src/jazz_elements/channel.h
@@ -253,37 +253,6 @@ class Channels : public Container {
 };
 typedef Channels *pChannels;
 
-// --------------------------------------------------------------------------------------------------------------------------------------------
-
-		// StatusCode url_put	   ();
-		// StatusCode url_get	   ();
-		// StatusCode url_delete  ();
-
-		// The shell
-
-// https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
-
-// #include <cstdio>
-// #include <iostream>
-// #include <memory>
-// #include <stdexcept>
-// #include <string>
-// #include <array>
-
-// std::string exec(const char* cmd) {
-//     std::array<char, 128> buffer;
-//     std::string result;
-//     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-//     if (!pipe) {
-//         throw std::runtime_error("popen() failed!");
-//     }
-//     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-//         result += buffer.data();
-//     }
-//     return result;
-// }
-
-		// StatusCode shell_exec  ();
 
 #ifdef CATCH_TEST
 
