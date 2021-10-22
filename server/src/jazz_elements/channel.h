b55774fe2878b62186ddd8d829a77193e65fdaf5
kaalam <kaalam@kaalam.ai>
2021-10-22 21:54:12 +0200

    Fixes doc

--- server/src/jazz_elements/channel.h
+++ server/src/jazz_elements/channel.h
@@ -67,12 +67,12 @@ namespace jazz_elements
 /// HttpQueryState apply values (on state == PSTATE_COMPLETE_OK)
 
 #define APPLY_NOTHING					 0		///< Just an l_value with {///node}//base/entity or {///node}//base/entity/key
-#define APPLY_NAME						 1		///< //base/entity/key:name (Select an item form a Tuple by name)
-#define APPLY_URL						 2		///< //base& any_url_encoded_url ; (A call to http or file)
+#define APPLY_NAME						 1		///< {///node}////base/entity/key:name (Select an item form a Tuple by name)
+#define APPLY_URL						 2		///< {///node}////base& any_url_encoded_url ; (A call to http or file)
 #define APPLY_FUNCTION					 3		///< {///node}//base/entity/key(//r_base/r_entity/r_key) (A function call on a block.)
-#define APPLY_FUNCT_CONST				 4		///< //base/entity/key(& any_url_encoded_const) (A function call on a const.)
+#define APPLY_FUNCT_CONST				 4		///< {///node}////base/entity/key(& any_url_encoded_const) (A function call on a const.)
 #define APPLY_FILTER					 5		///< {///node}//base/entity/key[//r_base/r_entity/r_key] (A filter on a block.)
-#define APPLY_FILT_CONST				 6		///< //base/entity/key[& any_url_encoded_const] (A filter on a const.)
+#define APPLY_FILT_CONST				 6		///< {///node}//base/entity/key[& any_url_encoded_const] (A filter on a const.)
 #define APPLY_RAW						 7		///< {///node}//base/entity/key.raw (Serialize text to raw.)
 #define APPLY_TEXT						 8		///< {///node}//base/entity/key.text (Serialize raw to text.)
 #define APPLY_ASSIGN_NOTHING			 9		///< {///node}//base/entity/key=//r_base/r_entity/r_key (Assign block to block.)
@@ -84,10 +84,10 @@ namespace jazz_elements
 #define APPLY_ASSIGN_FILT_CONST			15		///< {///node}//base/entity/key=//r_base/r_entity/r_key[& any_url_encoded_const]
 #define APPLY_ASSIGN_RAW				16		///< {///node}//base/entity/key=//r_base/r_entity/r_key.raw
 #define APPLY_ASSIGN_TEXT				17		///< {///node}//base/entity/key=//r_base/r_entity/r_key.text
-#define APPLY_ASSIGN_CONST				18		///< //base/entity/key=& any_url_encoded_const ; (Assign const to block.)
+#define APPLY_ASSIGN_CONST				18		///< {///node}//base/entity/key=& any_url_encoded_const ; (Assign const to block.)
 #define APPLY_NEW_ENTITY				19		///< {///node}//base/entity.new (Create a new entity)
 #define APPLY_GET_ATTRIBUTE				20		///< {///node}//base/entity/key.attribute(123) (read attribute 123 with HTTP_GET)
-#define APPLY_SET_ATTRIBUTE				21		///< //base/entity/key.attribute(123)=& url_encoded ; (set attribute 123 with HTTP_GET)
+#define APPLY_SET_ATTRIBUTE				21		///< {///node}////base/entity/key.attribute(46)=& url_encoded ; (set attrib. with HTTP_GET)
 #define APPLY_JAZZ_INFO					22		///< /// Show the server info.
 
 
