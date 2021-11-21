3b532c0a299fa02a0bbeb2cc00d7ce97ff88e8cf
kaalam <kaalam@kaalam.ai>
2021-11-21 19:46:20 +0100

    Starts the dowload script

---
+++ docker/tng/download_and_store.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd /home/jazz/jazz_dbg_mdb || return 1
+
+mkdir -p github_repos
+
+cd github_repos || return 1
+
+git clone git@github.com:kaalam/tng-data-facts.git
+git clone git@github.com:kaalam/tng-data-grammar.git
+git clone git@github.com:kaalam/tng-data-qa.git
+git clone git@github.com:kaalam/tng-data-readings.git
+git clone git@github.com:kaalam/tng-data-wikipedia.git
+
+cd tng-data-facts || return 1
+
+./expand.sh
+
+cd ../tng-data-grammar || return 1
+
+./expand.sh
+
+cd ../tng-data-qa || return 1
+
+./expand.sh
+
+cd ../tng-data-readings || return 1
+
+./expand.sh
+
+cd ../tng-data-wikipedia || return 1
+
+./expand.sh
+
+cd /home/jazz || return 1
+
